05a5fb70f0e3f276f5a1b0da71328ea175b3f59d
Maxime Minta <maxime.minta@hotmail.com>
2020-04-20 17:59:47 +0200

    Lijnencodevoorlopigincomment

---
+++ RESTful API MaximeMinta-v2/RESTful API MaximeMinta-v2/Controllers/ArtistController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RESTful_API_MaximeMinta_v2
+{
+    [Route("api/artists")]
+    public class ArtistController : Controller
+    {
+        private readonly SongLibraryDbContext library;
+
+        public ArtistController(SongLibraryDbContext library)
+        {
+            this.library = library;
+        }
+
+        [HttpGet] //api/artists
+        public List<Artist> GetAllArtists()
+        {
+            return library.Artists.ToList();
+        }
+
+        [HttpPost]
+        public IActionResult CreateArtist([FromBody] Artist newArtist)
+        {
+            library.Artists.Add(newArtist);
+            library.SaveChanges();
+            return Created("",newArtist);
+
+        }
+
+        [Route("id")]
+        [HttpGet]
+        public IActionResult GetArtist(int id)
+        {
+            var Artist = library.Artists.Find(id);
+            if (Artist == null)
+            {
+                return NotFound();
+            }
+            return Ok(Artist);
+        }
+
+        [Route("{id}")]
+        [HttpDelete]
+        public IActionResult DeleteArtist(int id)
+        {
+            var Artist = library.Artists.Find(id);
+            if (Artist == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                //verwijder Artist
+                library.Artists.Remove(Artist);
+                library.SaveChanges();
+
+                return NoContent(); // = de standaard response (204) bij een gelukte delete
+            }
+        }
+
+        [HttpPut]
+        public IActionResult UpdateTrack([FromBody] Artist UpdateArtist)
+        {
+            var OriginalArtist = library.Artists.Find(UpdateArtist.ArtistID);
+            if (OriginalArtist == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                OriginalArtist.Name = UpdateArtist.Name;
+                OriginalArtist.Tracks = UpdateArtist.Tracks;
+
+                library.SaveChanges();
+                return Ok(OriginalArtist);
+            }
+
+        }
+
+    }
+}
---
+++ RESTful API MaximeMinta-v2/RESTful API MaximeMinta-v2/Models/TrackArtist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RESTful_API_MaximeMinta_v2
+{
+    public class TrackArtist
+    {
+        public int TrackID { get; set; }
+        public Track Track { get; set; }
+
+        public int ArtistID { get; set; }
+        public Artist Artist { get; set; }
+
+    }
+}
